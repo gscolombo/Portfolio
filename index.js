91f8df69cfe455354b368bae92fb7626e8feff06
Gabriel <gabriel.souza.colombo13@gmail.com>
2022-09-09 10:39:32 -0300

    Entrypoint server port changes

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
  * Portfolio's server entrypoint
  */
 const server = require("./server");
-
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 server.listen(port, () => {
   console.log(`Server running at port ${port}`);
