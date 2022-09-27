1f003490c86f933ea3fba6563f2a5a812a1c36cd
Gabriel <gabriel.souza.colombo13@gmail.com>
2022-09-27 09:20:23 -0300

    Inclusion of body in e-mail return response

--- api/mail.js
+++ api/mail.js
@@ -4,66 +4,70 @@
 import nodemailer from "nodemailer";
 
 const options = {
-    host: 'smtp.gmail.com',
-    secure: true,
-    auth: {
-        user: 'gscolombo404@gmail.com',
-        pass: process.env.GMAIL_PASSWORD,
-    }
-}
+  host: "smtp.gmail.com",
+  secure: true,
+  auth: {
+    user: "gscolombo404@gmail.com",
+    pass: process.env.GMAIL_PASSWORD,
+  },
+};
 
 let transporter = nodemailer.createTransport(options);
 
 exports.handler = async (event) => {
-    const messageData = JSON.parse(event.body);
+  const messageData = JSON.parse(event.body);
 
-    let response = {
-        statusCode: null,
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8'
-        }
+  let response = {
+    statusCode: null,
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: {},
+  };
+
+  const connectionSuccess = new Promise((resolve) => {
+    transporter.verify((error) => {
+      if (error) {
+        resolve(false);
+      } else {
+        resolve(true);
+      }
+    });
+  });
+
+  if (await connectionSuccess) {
+    const message = {
+      from: messageData.email,
+      to: {
+        name: "Gabriel Colombo",
+        address: "gscolombo404@gmail.com",
+      },
+      replyTo: {
+        name: messageData.name,
+        address: messageData.email,
+      },
+      subject: "Contato - Website pessoal",
+      text: messageData.message,
+      priority: "high",
+      headers: {
+        "Reply-To": messageData.email,
+      },
     };
-    
-    const connectionSuccess = new Promise((resolve) => {
-        transporter.verify((error) => {
-            if (error) {
-                resolve(false)
-            } else {
-                resolve(true);
-            }
-        })
-    })
-        
-    if (await connectionSuccess) {
-        const message = {
-            from: messageData.email,
-            to: {
-                name: "Gabriel Colombo",
-                address: 'gscolombo404@gmail.com'
-            },
-            replyTo: {
-                name: messageData.name,
-                address: messageData.email
-            },
-            subject: "Contato - Website pessoal",
-            text: messageData.message,
-            priority: 'high',
-            headers: {
-                'Reply-To': messageData.email
-            }
 
-        }
-        
-        const res = await transporter.sendMail(message);
-        if (res.response.includes("OK")) {
-            response.statusCode = 200;
-        } else {
-            response.statusCode = 500;
-        }
+    const info = await transporter.sendMail(message);
+    if (info.response.includes("OK")) {
+      response.body.message = "Mensagem enviada com sucesso!";
+      response.body.info = info;
+      response.statusCode = 200;
     } else {
-        response.statusCode = 500;
-        response.error = "Não foi possível conectar ao servidor SMTP."
+      response.body.error = info;
+      response.statusCode = 500;
     }
-    
-    return response;
-}
+  } else {
+    response.statusCode = 500;
+    response.body.error = "Não foi possível conectar ao servidor SMTP.";
+  }
+
+  response.body = JSON.stringify(response.body);
+  return response;
+};
